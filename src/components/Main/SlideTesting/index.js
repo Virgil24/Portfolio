4dee2f3b8176769b6e6e822ddac9da20407418b1
Virgil <virgil@macbook-pro-de-virgil.home>
2021-03-24 11:15:49 +0100

    about slide finished

--- src/components/Main/SlideTesting/index.js
+++ src/components/Main/SlideTesting/index.js
@@ -10,14 +10,15 @@ const SlideTesting = ({ launchAnimation, animate }) => {
   };
   return (
     <div>
-      <div className="test__slide" onMouseOver={launchAnimationOnMouseOver}>
-        <section className="section section1">
-          <div className="box">
-            <div className="contains">
-              <div className="imgBx image1">
-                <img className="pic" src="https://images.unsplash.com/photo-1514467911470-2b2492c64477?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8bW91dGFpbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt="mountainpic" />
+      <div className="slideAbout" onMouseOver={launchAnimationOnMouseOver}>
+        <section className="slideAbout__section">
+          <h2 className="slideAbout__section__title slideAbout__section__title1"> À propos</h2>
+          <div className="slideAbout__section__box">
+            <div className="slideAbout__section__box__contains">
+              <div className="slideAbout__section__box__contains__imgBx slideAbout__section__box__contains__image1">
+                <img className="slideAbout__section__box__contains__imgBx__pic" src="https://images.unsplash.com/photo-1514467911470-2b2492c64477?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8bW91dGFpbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt="mountainpic" />
               </div>
-              <div className="contentt content1">
+              <div className="slideAbout__section__box__contains__contentt slideAbout__section__box__contains__content1">
                 {animate && (
                 <Typewriter
                   wrapperClassName="content__text"
@@ -35,25 +36,40 @@ const SlideTesting = ({ launchAnimation, animate }) => {
             </div>
           </div>
         </section>
-        <section className="section section2">
-          <div className="box">
-            <div className="contains">
-              <div className="imgBx image2">
-                <img className="pic" src="https://images.unsplash.com/photo-1494768788897-7fdaa00de2a1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8YmxhY2slMjBhbmQlMjB3aGl0ZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt="iceberg" />
+        <section className="slideAbout__section">
+          <h2 className="slideAbout__section__title slideAbout__section__title2"> Front-end</h2>
+          <div className="slideAbout__section__box">
+            <div className="slideAbout__section__box__contains">
+              <div className="slideAbout__section__box__contains__imgBx slideAbout__section__box__contains__image2">
+                <img className="slideAbout__section__box__contains__imgBx__pic" src="https://images.unsplash.com/photo-1494768788897-7fdaa00de2a1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8YmxhY2slMjBhbmQlMjB3aGl0ZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt="iceberg" />
               </div>
-              <div className="contentt content2">
+              <div className="slideAbout__section__box__contains__contentt slideAbout__section__box__contains__content2">
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore laudantium voluptatibus aspernatur, voluptates excepturi quis minima harum odit molestiae minus voluptatum mollitia ea, quae quidem doloribus assumenda dolore expedita qui?</p>
               </div>
             </div>
           </div>
         </section>
-        <section className="section section3">
-          <div className="box">
-            <div className="contains">
-              <div className="imgBx image3">
-                <img className="pic" src="https://images.unsplash.com/photo-1487766036723-04aebf785670?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fGJsYWNrJTIwYW5kJTIwd2hpdGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt="iceberg" />
+        <section className="slideAbout__section">
+          <h2 className="slideAbout__section__title slideAbout__section__title3"> Back-end</h2>
+          <div className="slideAbout__section__box">
+            <div className="slideAbout__section__box__contains">
+              <div className="slideAbout__section__box__contains__imgBx slideAbout__section__box__contains__image3">
+                <img className="slideAbout__section__box__contains__imgBx__pic" src="https://images.unsplash.com/photo-1487766036723-04aebf785670?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fGJsYWNrJTIwYW5kJTIwd2hpdGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt="iceberg" />
               </div>
-              <div className="contentt content3">
+              <div className="slideAbout__section__box__contains__contentt slideAbout__section__box__contains__content3">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore laudantium voluptatibus aspernatur, voluptates excepturi quis minima harum odit molestiae minus voluptatum mollitia ea, quae quidem doloribus assumenda dolore expedita qui?</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="slideAbout__section">
+          <h2 className="slideAbout__section__title slideAbout__section__title4"> Organisation</h2>
+          <div className="slideAbout__section__box">
+            <div className="slideAbout__section__box__contains">
+              <div className="slideAbout__section__box__contains__imgBx slideAbout__section__box__contains__image4">
+                <img className="slideAbout__section__box__contains__imgBx__pic" src="https://images.unsplash.com/photo-1487766036723-04aebf785670?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fGJsYWNrJTIwYW5kJTIwd2hpdGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt="iceberg" />
+              </div>
+              <div className="slideAbout__section__box__contains__contentt slideAbout__section__box__contains__content4">
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore laudantium voluptatibus aspernatur, voluptates excepturi quis minima harum odit molestiae minus voluptatum mollitia ea, quae quidem doloribus assumenda dolore expedita qui?</p>
               </div>
             </div>
