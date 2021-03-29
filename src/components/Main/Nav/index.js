79d4b6743c4ab7a9cdaa7e44e3e3f95fb4490bd8
Virgil <virgil@macbook-pro-de-virgil.home>
2021-03-29 12:03:02 +0200

    responsive 1200 to 768

--- src/components/Main/Nav/index.js
+++ src/components/Main/Nav/index.js
@@ -7,39 +7,39 @@ const Nav = ({ handleToggleOnClick, toggle }) => {
   const switchToggleOnClick = () => {
     handleToggleOnClick();
   };
-  // const checkWindowSize = window.matchMedia('(max-width: 768px)');
+  const checkWindowSize = window.matchMedia('(max-width: 768px)');
   // if the device size is over ipad do not display toggle menu
-  // if (checkWindowSize.matches === true) {
+  if (checkWindowSize.matches === true) {
     return (
       <nav>
-        {/* {toggle && ( */}
+        {toggle && (
         <nav className="navigation">
           <div className="navigation__marker" />
-          {/* <a href="#" className="navigation__item__icon"><FontAwesomeIcon icon={faTimesCircle} onClick={switchToggleOnClick} /></a> */}
+          <a href="#" className="navigation__item__icon"><FontAwesomeIcon icon={faTimesCircle} onClick={switchToggleOnClick} /></a>
           <a href="#presentation" className="navigation__item">Accueil</a>
           <a href="#projects" className="navigation__item">Projets</a>
           <a href="#about" className="navigation__item">Me concernant</a>
           <a href="#contact" className="navigation__item">Contact</a>
         </nav>
-        {/* )} */}
-        {/* {!toggle && ( */}
-        {/* <a href="#" className="navigation__item__icon"> <FontAwesomeIcon icon={faChevronCircleDown} onClick={switchToggleOnClick} /></a>
-        )} */}
+        )}
+        {!toggle && (
+        <a href="#" className="navigation__item__icon"> <FontAwesomeIcon icon={faChevronCircleDown} onClick={switchToggleOnClick} /></a>
+        )}
       </nav>
 
     );
     // if the device size is under ipad display toggle menu
-  // } if (checkWindowSize.matches === false) {
-  //   return (
-  //     <div className="navigation">
-  //       <a href="#presentation" className="navigation__item">Accueil</a>
-  //       <a href="#projects" className="navigation__item">Projets</a>
-  //       <a href="#about" className="navigation__item">Me concernant</a>
-  //       <a href="#contact" className="navigation__item">Contact</a>
+  } if (checkWindowSize.matches === false) {
+    return (
+      <div className="navigation">
+        <a href="#presentation" className="navigation__item">Accueil</a>
+        <a href="#projects" className="navigation__item">Projets</a>
+        <a href="#about" className="navigation__item">Me concernant</a>
+        <a href="#contact" className="navigation__item">Contact</a>
 
-  //     </div>
-  //   );
-  // }
+      </div>
+    );
+  }
 };
 
 export default Nav;
